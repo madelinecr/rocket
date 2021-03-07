22b98166523ab06903a27444fce2c6da3b8526f6
Avery Pace <blainepace@gmail.com>
2021-03-06 20:02:21 -0500

    Add resources :posts to routes, created posts controller with new and show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render action: "new"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
